842bf6de475f00e9f95abbc64d58fcad29756c2f
Simas Usas <simas.usas@instarem.com>
2019-12-18 16:11:44 +0200

    Add action creator didMount test

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import { storeFactory } from '../test/testUtils';
-import App from './App';
+import App, { UnconnectedApp } from './App';
 
 const setup = (state = {}) => {
     const store = storeFactory(state);
@@ -34,4 +34,18 @@ describe('redux props', () => {
         const getSecretWordProp = wrapper.instance().props.getSecretWord;
         expect(getSecretWordProp).toBeInstanceOf(Function);
     });
+});
+
+
+it('getSecretWord runs on App mount', () => {
+  const getSecretWordMock = jest.fn();
+  const props = {
+    getSecretWord: getSecretWordMock,
+    success: false,
+    guessedWords: [],
+  };
+  const wrapper = shallow(<UnconnectedApp {...props} />);
+  wrapper.instance().componentDidMount();
+  const getSecretWordCallCount = getSecretWordMock.mock.calls.length;
+  expect(getSecretWordCallCount).toBe(1);
 });
